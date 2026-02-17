f5b01470b3df4b6677dd83c4642d5eb03051f5b3
Azka Syakirah <azkasyakir800@gmail.comA>
2026-02-17 08:53:12 +0700

    creating model

---
+++ lib/models/task.dart
@@ -0,0 +1,8 @@
+class Task {
+  final String name;
+  final String priority;
+  final int duration;
+  final String deadline;
+
+  Task({required this.name, required this.priority, required this.duration, required this.deadline});
+}
